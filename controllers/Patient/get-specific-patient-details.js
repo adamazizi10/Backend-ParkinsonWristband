ca34cf2ced67eb613d8c8ca8afbbe4a77751d7ea
Adam Azizi <106051947+adamazizi10@users.noreply.github.com>
2024-05-07 14:58:33 -0400

    updates

--- controllers/Patient/get-specific-patient-details.js
+++ controllers/Patient/get-specific-patient-details.js
@@ -1,15 +1,15 @@
 const handleGetSpecificPatientDetails = (req, res, db) => {
     const patientId = req.params.id; // Get the patient ID from the URL parameter
 
-    // Query your database to retrieve patient details based on patientId
+   
     db('patient')
         .select('*', /* other patient details */)
-        .where('id', patientId) // Assuming your patient table has an 'id' column
+        .where('id', patientId)
         .then((patientDetails) => {
             if (patientDetails.length === 0) {
                 res.status(404).json({ error: 'Patient not found' });
             } else {
-                res.json(patientDetails[0]); // Assuming you expect only one patient with the given ID
+                res.json(patientDetails[0]); 
             }
         })
         .catch((error) => {
